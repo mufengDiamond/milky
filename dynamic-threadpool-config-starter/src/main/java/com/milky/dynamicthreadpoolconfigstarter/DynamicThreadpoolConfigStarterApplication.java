c6eb5f8e8b530e5b362e106ba6b69318b6984621
taomeng <jianhongyunfeng@qq.com>
2020-08-30 21:55:23 +0800

    firt commit

---
+++ dynamic-threadpool-config-starter/src/main/java/com/milky/dynamicthreadpoolconfigstarter/DynamicThreadpoolConfigStarterApplication.java
@@ -0,0 +1,13 @@
+package com.milky.dynamicthreadpoolconfigstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicThreadpoolConfigStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicThreadpoolConfigStarterApplication.class, args);
+	}
+
+}
---
+++ dynamic-threadpool-config-starter/src/main/java/com/milky/manager/CustomThreadPoolManager.java
@@ -0,0 +1,15 @@
+package com.milky.manager;
+
+/**
+ * @Auther: taomeng
+ * @Date: 2020/8/8 13:11
+ * @Version: V 1.0.0
+ * @Description: thread pool manager for developer
+ */
+public class CustomThreadPoolManager {
+
+
+
+
+
+}
